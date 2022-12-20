64f2986169406bd297b16bfecd58149601864574
Mohammed Mubashir <mohammedmubashir204@gmail.com>
2022-12-20 20:21:15 +0530

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Mohammed Mubashir on 20/12/22.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
